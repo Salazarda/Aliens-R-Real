d1af27ec175944b8fb787f1109a7bcf5a95e0500
Salazarda <46509817+Salazarda@users.noreply.github.com>
2019-06-18 16:36:11 -0400

    updated changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,38 @@
 // from data.js
 var tableData = data;
-
+console.log(tableData);
 // YOUR CODE HERE!
+
 var tbody = d3.select("tbody");
 
-console.log(data);
+function printUfos(tableData){
+    tableData.forEach((Ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(Ufo).forEach(([key, value]) => {
+            row.append("td").text(value);
+        });
 
-/////////////////////
-
-// function getUFOdata(tableData) {
-//     tableData.forEach(UFO) {
-//     var row = tbody.append("tr");
-//     Object.entries(UFO).forEach(([key, value]) => {
-//         row.append("td").text(value);
-//         });
-//     });
-// }
-
-
-
+    });
+}
+//////////
 
 var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
-
+//   table.length = 0
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  
+
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  console.log(tableData);
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    printUfos(filteredData);
+    filteredData.length = [];
 
-  tableData.filter(x => x.datetime == inputValue).forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-    row.append("td").text(value);
-    });
 });
-console.log(tableData.filter(x => x.datetime == inputValue));
 
-  
-//   var filteredData = tableData.filter(spotting => spotting.datetime === inputValue);
-//   getUFOdata(filteredData);
-//   filteredData.length = [];
 
-  // clear the input value
- // d3.select("#datetime").node().value = "";
-});
